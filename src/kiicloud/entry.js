58b9431fac7177b345045ed5f947be550da417a1
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-11-18 08:08:58 +0900

    Add a hook to handle GROUP_MEMBERS_ADDED

--- src/kiicloud/entry.js
+++ src/kiicloud/entry.js
@@ -1,8 +1,10 @@
 "strict"
 import { config } from "./config";
 import { userCreated } from "./user-created";
+import { groupMembersAdded } from "./group-members-added";
 
 (function(exports) {
   exports.join = config;
   exports.userCreated = userCreated;
+  exports.groupMembersAdded = groupMembersAdded;
 })(global);
---
+++ src/kiicloud/group-members-added.js
@@ -0,0 +1,31 @@
+/*
+ *
+ */
+export function groupMembersAdded(params, ctx, done) {
+  const admin = ctx.getAppAdminContext();
+  admin.groupWithID(params.groupID)
+    .listTopics()
+    .then(args => {
+      const topics = args[0];
+      return Promise.all(topics.map(t => {
+        const m = {type: "GROUP-MEMBERS-ADDED", members: params.members};
+        const data = {value: JSON.stringify(m)};
+        const msg = new KiiPushMessageBuilder(data).build()
+        return t.sendMessage(msg)
+      }));
+    })
+    .then(_ => done(_))
+    .catch(err => {
+      done(err.message);
+    })
+}
+/*
+{
+  "groupID": "kiicorp",
+  "members": [
+    "3c794b2c5ea0-5f19-6e11-01da-094966ec"
+  ],
+  "failed": [],
+  "uri": "kiicloud://groups/kiicorp"
+}
+*/
