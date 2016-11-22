369a2db3ec6d16f2949ddbaf9a0195c9c15b8f05
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-11-22 23:51:15 +0900

    Enable menu if not debug

--- main.js
+++ main.js
@@ -38,30 +38,32 @@ app.on('ready', () => {
 
   win.loadURL(`file://${__dirname}/index.html`);
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate([{
-    //label: "Application",
-    submenu: [
-      //{ label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-      //{ type: "separator" },
-      { label: "Quit", accelerator: "Command+Q", click: () => app.quit()}
-    ]}, {
-    label: "Edit",
-    submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]}
-  ]))
+  if (!debug) {
+    Menu.setApplicationMenu(Menu.buildFromTemplate([{
+      //label: "Application",
+      submenu: [
+        //{ label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        //{ type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: () => app.quit()}
+      ]}, {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" },
+      ]}
+    ]))
+  }
 
   //win.webContents.openDevTools();
   win.webContents.on('did-finish-load', () => {
   });
 
-  if (global.config.debug) {
+  if (debug) {
     const loadDevtool = require('electron-load-devtool');
     loadDevtool(loadDevtool.REDUX_DEVTOOLS);
     loadDevtool(loadDevtool.REACT_DEVELOPER_TOOLS);
